fa55f1d0055157aec00ec12a99a0495784edcf1e
MohamedNadeem-BH <73463175+MohamedNadeem-BH@users.noreply.github.com>
2020-12-15 14:20:40 +0100

    Update QR Code in Revit.cs

--- QR Code in Revit/QR Code in Revit.cs
+++ QR Code in Revit/QR Code in Revit.cs
@@ -20,77 +20,57 @@ namespace Github_QRCode
     [Regeneration(RegenerationOption.Manual)]
     public class Class1 : IExternalCommand
     {
-        public string qrCodeText(Document doc, Element elem)
+        public string encodeCoordinates(XYZ inCoordinates)
+        {
+            double factor = 1000;
+
+            //Shared coordinates of point
+            int xCoord = (int)Math.Round(inCoordinates.X * factor, 0);
+            int yCoord = (int)Math.Round(inCoordinates.Y * factor, 0);
+            int zCoord = (int)Math.Round(inCoordinates.Z * factor, 0);
+            string E = xCoord.ToString().PadLeft(7, '0');
+            string N = yCoord.ToString().PadLeft(7, '0');
+            string Z = zCoord.ToString().PadLeft(7, '0');
+
+            return "E" + E + "N" + N + "Z" + Z;
+        }
+
+        public string qrCodeText(Document doc, Element elem, XYZ internalOrigin, double internalRotation)
         // Get QR Code text from Element location (shared coordinates)
         // QR Code text format: E2547858580N1122112220Z0426900
         // For E= 2547858.58, N = 1122112.22, Z=426.9
         //E##########N##########Z###### East(10)+North(10)+Elevation(7)
         {
             #region Variables definitions
-            //Project base points
-            var basePoints = new FilteredElementCollector(doc).OfClass(typeof(BasePoint)).ToElements();
-            var projectPoint = basePoints.First(
-                x => (x.Category.Id.IntegerValue == (int)BuiltInCategory.OST_ProjectBasePoint));
-            //Base point main parameter values
-            double ns = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_NORTHSOUTH_PARAM).AsDouble();
-            double ew = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_EASTWEST_PARAM).AsDouble();
-            double ele = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_ELEVATION_PARAM).AsDouble();
-            double rot = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_ANGLETON_PARAM).AsDouble();
-            ProjectPosition projPos = (doc).ActiveProjectLocation.GetProjectPosition(XYZ.Zero);
-            XYZ iop = new XYZ(projPos.EastWest, projPos.NorthSouth, 0);
-            XYZ pbp = new XYZ(ew, ns, ele);
-            XYZ lcP= new XYZ(0,0,0);
+            XYZ locationPoint;
             //Other definitions
             double ft2m = 0.3048; // for unit conversion
-            string xCoord, yCoord, zCoord;
             string qrTextOut;
-            double z0;
             Transform rotationTransform1;
             #endregion
 
             #region QR Code information
             //Coordinate conversion
             rotationTransform1 =
-                Transform.CreateRotationAtPoint(XYZ.BasisZ, (rot * -1), iop);
+                Transform.CreateRotationAtPoint(XYZ.BasisZ, (internalRotation * -1), internalOrigin);
             //Get element location
             if (null != (elem.Location as LocationPoint))
             {
-                lcP = (elem.Location as LocationPoint).Point;
+                locationPoint = (elem.Location as LocationPoint).Point;
             } else if (null != (elem.Location as LocationCurve))
             {
                 Curve lcC = (elem.Location as LocationCurve).Curve;
-                lcP = (lcC.GetEndPoint(0)+(lcC.GetEndPoint(1))).Multiply(0.5); // curve midpoint
+                locationPoint = (lcC.GetEndPoint(0)+(lcC.GetEndPoint(1))).Multiply(0.5); // curve midpoint
             }
             else
             {
                 return "No valid location found for element number: "+elem.Id.IntegerValue;
             }
-            lcP = new XYZ(lcP.X + iop.X, lcP.Y + iop.Y, lcP.Z + iop.Z);
-            lcP = rotationTransform1.OfPoint(lcP);
-            lcP = new XYZ(lcP.X, lcP.Y, lcP.Z + pbp.Z);
-            lcP = new XYZ(lcP.X * ft2m, lcP.Y * ft2m, lcP.Z * ft2m);
-            //Shared coordinates of point
-            xCoord = Math.Round(lcP.X, 3).ToString();
-            yCoord = Math.Round(lcP.Y, 3).ToString();
-            zCoord = Math.Round(lcP.Z, 3).ToString();
-            z0 = Math.Round(lcP.Z, 3);
-            //QR Code text
+            locationPoint = locationPoint + internalOrigin;
+            locationPoint = rotationTransform1.OfPoint(locationPoint);
+            locationPoint.Multiply(ft2m);
 
-            if (z0 < 10)// z coordinate should have exactly 7 digits ex z=5.203 becomes 0005203
-            {
-                zCoord = "000" + z0;
-            }
-            else if (z0 < 100)
-            {
-                zCoord = "00" + z0;
-            }
-            else if (z0 < 1000)
-            {
-                zCoord = "0" + z0;
-            }
-            qrTextOut = "E" + xCoord + "N" + yCoord + "Z" + zCoord;
-            qrTextOut = qrTextOut.Replace(",", "");
-            qrTextOut = qrTextOut.Replace(".", "");
+            qrTextOut = encodeCoordinates(locationPoint);
             #endregion
 
             return qrTextOut;
@@ -124,6 +104,12 @@ namespace Github_QRCode
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
+            //Project base points
+            var projectPoint = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ProjectBasePoint).ToElements().FirstOrDefault();
+            double internalRotation = projectPoint.get_Parameter(BuiltInParameter.BASEPOINT_ANGLETON_PARAM).AsDouble();
+            ProjectPosition projPos = (doc).ActiveProjectLocation.GetProjectPosition(XYZ.Zero);
+            XYZ internalOrigin = new XYZ(projPos.EastWest, projPos.NorthSouth, projPos.Elevation);
+
             string imagesFolderPath = VerifyDir(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + (@"\QRcode_Images"));
             string myImagePath;
 
@@ -132,7 +118,7 @@ namespace Github_QRCode
             foreach (Element elem in pickedElements)
             {
                 myImagePath = imagesFolderPath + (@"\QR_") + elem.Id.ToString() + ".bmp";
-                createQRCode(qrCodeText(doc, doc.GetElement(elem.Id)), myImagePath);
+                createQRCode(qrCodeText(doc, doc.GetElement(elem.Id), internalOrigin, internalRotation), myImagePath);
             }
             TaskDialog.Show("Baslerhofmann", pickedElements.Count + " QR Code images created.");
             return Result.Succeeded;
